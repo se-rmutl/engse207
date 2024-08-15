22e63ea0a64bef229ec8ce9b70c3dc2f552ba357
Thanit Keatkaew <thanit@rmutl.ac.th>
2024-08-15 18:41:44 +0700

    index and main are changed

--- frontend/agent_notification/main.js
+++ frontend/agent_notification/main.js
@@ -5,7 +5,6 @@ const WindowManager = require('./Scripts/WindowManager.js');
 //Main Object responsible for managing the electron windows is created
  windowManager = new WindowManager();
 
-
 //Called when Electron is ready
 //This creates the browser windows and tray in the menu bar
 app.on('ready', windowManager.createUI.bind(windowManager));
@@ -16,6 +15,8 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+//app.commandLine.appendSwitch("ignore-certificate-errors");
 
 //-----------------------
 
