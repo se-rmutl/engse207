d1ed581d7cca3921ba4c8427a10b5387d9b677e8
Thanit Keatkaew <thanit@rmutl.ac.th>
2024-08-19 11:50:46 +0700

    Init for LAB3

--- backend/endpoint-api/sqlConfig.js
+++ backend/endpoint-api/sqlConfig.js
@@ -1,9 +1,24 @@
 var dbconfig = {
-    web_labDB: {
+    development: {
         server: '192.168.56.106',
         database:'team0_web_labDB',
         user:'sa',
-        password:'',
+        password:'P@ssw0rd',
+        port: 1433,
+        options:{
+            encript: true,
+            setTimeout: 12000,
+            enableArithAbort: true,
+            trustServerCertificate: true,
+            trustedconnection:  true,
+            instancename:  '192.168.56.106'  // SQL Server instance name
+        }
+    },
+    production: {
+        server: '192.168.56.106',
+        database:'team0_web_labDB',
+        user:'sa',
+        password:'P@ssw0rd',
         port: 1433,
         options:{
             encript: true,
