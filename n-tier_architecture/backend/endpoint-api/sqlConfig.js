025159024cda94e97aca3689d69c7cfb8ec76321
tkeatkaew <thanit@rmutl.ac.th>
2024-12-07 22:10:42 +0700

    First code changed

--- n-tier_architecture/backend/endpoint-api/sqlConfig.js
+++ n-tier_architecture/backend/endpoint-api/sqlConfig.js
@@ -1,9 +1,9 @@
 var dbconfig = {
-    web_labDB: {
-        server: localhost,
-        database:'team0_web_labDB',
-        user:'sa',
-        password:'',
+    development: {
+        server: '192.168.56.9',
+        database:'team1_engse207_db',
+        user:'team1',
+        password:'P@ssw0rd',
         port: 1433,
         options:{
             encript: true,
@@ -11,9 +11,25 @@ var dbconfig = {
             enableArithAbort: true,
             trustServerCertificate: true,
             trustedconnection:  true,
-            instancename:  localhost  // SQL Server instance name
+            instancename:  '192.168.56.106'  // SQL Server instance name
+        }
+    },
+    production: {
+        server: '10.21.43.203', //SE Lab Server
+        database:'team1_engse207_db',
+        user:'team1',
+        password:'P@ssw0rd',
+        port: 1433,
+        options:{
+            encript: true,
+            setTimeout: 12000,
+            enableArithAbort: true,
+            trustServerCertificate: true,
+            trustedconnection:  true,
+            instancename:  '10.21.43.203'  // SQL Server instance name
         }
     },
 
 };
+
 module.exports = dbconfig;
